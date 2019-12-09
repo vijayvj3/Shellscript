53e6b33c4d173b0a483a70bce5656cefdb4e4f53
Papamani <30549690+Papamani@users.noreply.github.com>
2019-12-09 03:40:15 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ yum update
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 yum install -y jenkins
+service jenkins start
