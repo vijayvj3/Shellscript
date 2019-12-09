1f6aa03f30d3102a586e16556c50726d342d7e28
Papamani <30549690+Papamani@users.noreply.github.com>
2019-12-08 23:33:00 -0800

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo su
+yum update
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+yum install -y jenkins
