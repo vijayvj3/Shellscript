0d7342c77241ac21adcdf2a62eed9da4daab25c2
Papamani <30549690+Papamani@users.noreply.github.com>
2019-12-09 22:42:28 -0800

    Update mvn.sh

--- mvn.sh
+++ mvn.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
 
 sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
