2d580c11bbf0b48e8b41e58b466258156934f6d3
vijayvj3 <55652667+vijayvj3@users.noreply.github.com>
2020-02-27 11:16:04 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,7 +2,7 @@
 sudo su
 sudo  yum update -y
 yum install java-1.8.0
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+wget -O http://mirrors.jenkins.io/war/latest/jenkins.war
 rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 yum install -y jenkins
 service jenkins start
